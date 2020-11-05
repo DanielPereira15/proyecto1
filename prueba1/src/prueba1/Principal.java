ef2d917269e439735a104185f189f2b9aad5d0b0
bt <bt@PC-3.mshome.net>
2020-11-05 21:32:07 +0100

    commit dos

--- prueba1/src/prueba1/Principal.java
+++ prueba1/src/prueba1/Principal.java
@@ -4,7 +4,9 @@ public class Principal {
 
 	public static void main(String[] args) {
 		System.out.println("HOLA MUNDO java-git");
-
+		System.out.println("HOLA MUNDO java-git2");
+		System.out.println("HOLA MUNDO java-git3");
+		System.out.println("HOLA MUNDO java-git4");
 	}
 
 }
